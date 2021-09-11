dac5e47c9d676fd58fafe308388d218fd5e60b4c
Safir Medjeber <smedjeber.prestataire@oui.sncf>
2021-09-11 22:51:56 +0200

    refacto

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -129,39 +129,10 @@ public class Yatzy {
     }
 
     public  int fullHouse() {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-        tallies = new int[6];
-        tallies[dices[0] - 1] += 1;
-        tallies[dices[1] - 1] += 1;
-        tallies[dices[2] - 1] += 1;
-        tallies[dices[3] - 1] += 1;
-        tallies[dices[4] - 1] += 1;
-
-        for (i = 0; i != 6; i += 1) {
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i + 1;
-            }
-        }
-
-        for (i = 0; i != 6; i += 1) {
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i + 1;
-            }
-        }
-
-        if (_2 && _3) {
-            return _2_at * 2 + _3_at * 3;
-        }
-        else {
-            return 0;
-        }
+        int pair = score_pair();
+        int three = three_of_a_kind();
+        if( pair + three == Arrays.stream(dices).sum())
+            return pair + three;
+        return 0;
     }
 }
